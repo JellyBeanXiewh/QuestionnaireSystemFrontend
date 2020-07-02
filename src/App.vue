7844354602a4d56d2f7772de1525752238eda738
JellyBeanXiewh <447037959@qq.com>
2020-07-02 22:54:54 +0800

    删除无用代码

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,13 @@
 <template>
   <div id="app">
-<!--    <img alt="Vue logo" src="./assets/logo.png">-->
-<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-<!--    <Header></Header>-->
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-// import Header from "./components/Header";
 
 export default {
   name: 'App',
-  // components: {
-  //   Header,
-  // }
 }
 </script>
 
